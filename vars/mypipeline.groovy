1250e104d6bb12d7fd50e81bbb60c6e531e168d1
gayendama <117781785+gayendama@users.noreply.github.com>
2024-07-26 16:18:25 +0000

    Update mypipeline.groovy

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -12,7 +12,7 @@ def call() {
 
     stage ('Build docker images') {
             buildDockerImage(p.IMAGE_NAME, p.DOCKER_ID)
-        }
+    }
     stage ('test d'acceptence') {
             testAcceptance(p.IMAGE_NAME, p.DOCKER_ID, p.PortApp, p.PortContainer)
         }
