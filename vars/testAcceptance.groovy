7436bb96e3936001327367580667c92f18764a1c
gayendama <117781785+gayendama@users.noreply.github.com>
2024-07-26 22:09:44 +0000

    Update testAcceptance.groovy

--- vars/testAcceptance.groovy
+++ vars/testAcceptance.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def  call(String param1, String param2, int param3, int param4) {
-    sh "docker run --name ${param1} -d -p ${param2}:${param3} ${param4}:latest"
+    sh "docker run --name ${param1} -d -p ${param3}:${param4} ${param1}:latest"
     sleep 10
    
 }
