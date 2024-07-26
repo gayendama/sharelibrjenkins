742808e8469356ac893a10d903db691cdbc2c12e
gayendama <117781785+gayendama@users.noreply.github.com>
2024-07-26 13:45:44 +0000

    Update pipelineConfig.groovy

--- vars/pipelineConfig.groovy
+++ vars/pipelineConfig.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-    Map pipelineConfig = readYaml(file: "${Workspaces}/pipeline.yaml")
+    Map pipelineConfig = readYaml(file: "${workspace}/pipeline.yaml")
     return pipelineConfig
 }
