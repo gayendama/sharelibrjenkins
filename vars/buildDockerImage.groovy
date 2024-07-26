54d7bf7b4e1c71ee32383fe6036f9dd3ff59223e
gayendama <117781785+gayendama@users.noreply.github.com>
2024-07-26 15:29:17 +0000

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String param) {
-    sh "sudo docker build -t ${param}/${param}:latest ."
+def call(String param1, String param2) {
+    sh "sudo docker build -t ${param2}/${param1}:latest ."
 }
