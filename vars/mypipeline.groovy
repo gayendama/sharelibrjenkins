017bd08fa96557dba7ecff30b654dfd7735ac35a
gayendama <117781785+gayendama@users.noreply.github.com>
2024-07-27 00:50:30 +0000

    Update mypipeline.groovy

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -17,9 +17,8 @@ def call() {
             testAcceptance(p.IMAGE_NAME, p.DOCKER_ID, p.PortContainer, p.PortApp)
    }
    stage('Run JMeter Tests') {
-                steps {
-                    testDePerformance()
-                }
+                testDePerformance()
+                
             }
     }
 }
