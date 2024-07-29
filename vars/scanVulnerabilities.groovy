9e2b0b209102be7a985499b38f8fa5f76fe570ca
gayendama <ndamagaye64@gmail.com>
2024-07-29 10:34:57 +0000

    add methode scan vulnerablite

---
+++ vars/scanVulnerabilities.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+def call (String param) {
+    // Scanner l'image Docker avec Trivy
+    sh "trivy image --no-progress --exit-code 1 --severity HIGH ${param}:latest"
+    sleep 10
+}
