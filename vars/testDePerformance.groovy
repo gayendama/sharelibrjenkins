8d12135f70992376e32edcb745f364cb7115a547
gayendama <117781785+gayendama@users.noreply.github.com>
2024-07-29 09:46:13 +0000

    Update testDePerformance.groovy

--- vars/testDePerformance.groovy
+++ vars/testDePerformance.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 def testDePerformance(String param) {
-   sh 'sudo /home/ndama/jmeter/apache-jmeter-5.6.3/bin/jmeter -n -t testPlan.jmx -l results.jtl'
+   sh 'sudo ${param}/bin/jmeter -n -t testPlan.jmx -l results.jtl'
    sh 'cat results.jtl'
    perfReport 'results.jtl'
-}
+} 
 
         
