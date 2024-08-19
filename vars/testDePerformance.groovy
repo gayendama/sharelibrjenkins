615743a1eb4c8edf3534562139d116e8b47b8651
gayendama <117781785+gayendama@users.noreply.github.com>
2024-08-19 15:31:09 +0000

    Update testDePerformance.groovy

--- vars/testDePerformance.groovy
+++ vars/testDePerformance.groovy
@@ -1,8 +1,13 @@
 #!/usr/bin/env groovy
 def call() {
+   def jmxFile = 'testPlan.jmx'
+   if (fileExists(jmxFile)) {
    sh 'sudo /home/ndama/jmeter/apache-jmeter-5.6.3/bin/jmeter -n -t testPlan.jmx -l results.jtl'
    sh 'cat results.jtl'
    perfReport 'results.jtl'
+    } else {
+        echo "Le fichier ${jmxFile} n'existe pas, aucun test ne sera exécuté."
+    }
 } 
 
         
