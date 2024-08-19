7dda64b32046b54f9edc86461954da0580e3ac92
gayendama <117781785+gayendama@users.noreply.github.com>
2024-08-19 15:56:41 +0000

    Update testDePerformance.groovy

--- vars/testDePerformance.groovy
+++ vars/testDePerformance.groovy
@@ -1,13 +1,17 @@
 #!/usr/bin/env groovy
 def call() {
-   def jmxFile = 'testPlan.jmx'
-   if (fileExists(jmxFile)) {
-   sh 'sudo /home/ndama/jmeter/apache-jmeter-5.6.3/bin/jmeter -n -t testPlan.jmx -l results.jtl'
-   sh 'cat results.jtl'
-   perfReport 'results.jtl'
-    } else {
-        echo "Le fichier ${jmxFile} n'existe pas, aucun test ne sera exécuté."
-    }
-} 
+    def appDir = '/var/lib/jenkins/workspace/testperfomance/' 
+    def jmxFile = ''
 
-        
+    // Rechercher un fichier .jmx dans le répertoire de l'application
+    def files = new File(appDir).listFiles().find { it.name.endsWith('.jmx') }
+
+    if (files) {
+        jmxFile = files.name
+        echo "Fichier .jmx trouvé : ${jmxFile}"
+        sh 'sudo /home/ndama/jmeter/apache-jmeter-5.6.3/bin/jmeter  -n -t ${appDir}${jmxFile} -l results.jtl"
+        perfReport 'results.jtl'
+    } else {
+        echo "Aucun fichier .jmx trouvé dans le répertoire ${appDir}, aucun test ne sera exécuté."
+    }
+}
