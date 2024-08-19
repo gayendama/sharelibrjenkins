01420407687108e9000c0030fd9e6348bedb7a07
gayendama <117781785+gayendama@users.noreply.github.com>
2024-08-19 16:28:50 +0000

    Update testDePerformance.groovy

--- vars/testDePerformance.groovy
+++ vars/testDePerformance.groovy
@@ -6,7 +6,7 @@ def call() {
 
      if (jmxFile) {
         echo "Fichier .jmx trouvé : ${jmxFile}"
-        sh "sudo /home/ndama/jmeter/apache-jmeter-5.6.3/bin/jmeter  -n -t ${appDir}${jmxFile} -l results.jtl"
+        sh "sudo /home/ndama/jmeter/apache-jmeter-5.6.3/bin/jmeter  -n -t ${jmxFile} -l results.jtl"
         perfReport 'results.jtl'
     } else {
         echo "Aucun fichier .jmx trouvé dans le répertoire ${appDir}, aucun test ne sera exécuté."
