f1a6ac5445d2e71c310208aa927c9e8f6999a8ab
gayendama <117781785+gayendama@users.noreply.github.com>
2024-08-29 12:37:15 +0000

    Update runSoapUITests.groovy

--- vars/runSoapUITests.groovy
+++ vars/runSoapUITests.groovy
@@ -7,12 +7,12 @@ def call() {
     sh "mkdir -p ${resultsDir}"
 
     // Exécuter SoapUI et générer les rapports HTML
-    sh "/opt/SmartBear/SoapUI-5.7.2/bin/testrunner.sh -r -j -f${resultsDir}  calculator-soapui-project.xml"
+    sh "/opt/SmartBear/SoapUI-5.7.2/bin/testrunner.sh -r -j -f ${resultsDir}  calculator-soapui-project.xml"
     publishHTML(target: [
         allowMissing: false,
         keepAll: true,
         reportDir: "${resultsDir}",
-        reportFiles: 'TEST-CalculatorTestSuite.xml',
+        reportFiles: '*.xml',
         reportName: 'Rapport de Test Soapui'
     ])
     // Archiver les rapports
