bb48183215e2ce2b98656a0043732484f0217278
gayendama <117781785+gayendama@users.noreply.github.com>
2024-09-01 22:53:28 +0000

    Update runSoapUITests.groovy

--- vars/runSoapUITests.groovy
+++ vars/runSoapUITests.groovy
@@ -2,19 +2,24 @@
 def call() {
     def appDir = "${env.WORKSPACE}/" 
     def resultsDir = "${appDir}soapui-reports"
-
+    // Chemin vers le répertoire contenant les fichiers SoapUI
+    def soapuiDir = "${appDir}testsoapui"
     // Créer le répertoire pour les rapports s'il n'existe pas
     sh "mkdir -p ${resultsDir}"
-
-    // Exécuter SoapUI et générer les rapports HTML
-    sh "/opt/SmartBear/SoapUI-5.7.2/bin/testrunner.sh -r -j -f ${resultsDir}  calculator-soapui-project.xml"
-    publishHTML(target: [
-        allowMissing: false,
-        keepAll: true,
-        reportDir: "${resultsDir}",
-        reportFiles: '*.xml',
-        reportName: 'Rapport de Test Soapui'
-    ])
-    // Archiver les rapports
-  //  archiveArtifacts artifacts: "${resultsDir}"/*.xml", allowEmptyArchive: true
+    
+    // Trouver tous les fichiers .xml dans le répertoire de tests et les exécuter
+    def soapuiFiles = sh(script: "find ${soapuiDir} -name '*.xml'", returnStdout: true).trim().split('\n')
+    
+    if (soapuiFiles.size() == 0) {
+        echo "Aucun fichier SoapUI trouvé dans ${soapuiDir}."
+        return
+    }
+    
+    // Exécuter SoapUI pour chaque fichier trouvé
+    soapuiFiles.each { file ->
+        echo "Exécution des tests SoapUI pour ${file}..."
+        sh "/opt/SmartBear/SoapUI-5.7.2/bin/testrunner.sh -r -j -f ${resultsDir} ${file}"
+    }
 }
+
+
