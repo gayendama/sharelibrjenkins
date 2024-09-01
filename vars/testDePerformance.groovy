b852062e725ed35a7f11d55ee142ca3366f5802b
gayendama <117781785+gayendama@users.noreply.github.com>
2024-09-01 23:36:41 +0000

    Update testDePerformance.groovy

--- vars/testDePerformance.groovy
+++ vars/testDePerformance.groovy
@@ -1,14 +1,24 @@
 #!/usr/bin/env groovy
 def call() {
     def appDir = "${env.WORKSPACE}/" 
-   
-    def jmxFile = sh(script: "ls ${appDir}*.jmx | head -n 1", returnStdout: true).trim()
+     def jmxDir = "${appDir}/testjmeter"
 
-     if (jmxFile) {
-        echo "Fichier .jmx trouvé : ${jmxFile}"
-        sh "sudo /home/ndama/jmeter/apache-jmeter-5.6.3/bin/jmeter  -n -t ${jmxFile} -l results.jtl"
-        perfReport 'results.jtl'
-    } else {
-        echo "Aucun fichier .jmx trouvé dans le répertoire ${appDir}, aucun test ne sera exécuté."
+    // Vérifier si le répertoire de tests existe
+    if (!fileExists(jmxDir)) {
+        error "Le répertoire des tests Jmter ${jmxDir} n'existe pas."
+    }
+   
+    def jmxFiles = sh(script: "find ${jmxDir} -name '*.jmx'", returnStdout: true).trim().split('\n')
+    if (jmxFiles.size() == 0) {
+        echo "Aucun fichier SoapUI trouvé dans ${jmxDir}."
+        return
+    }
+      // Exécuter JMeter pour chaque fichier trouvé
+    jmxFiles.each { file ->
+        echo "Exécution des tests JMeter pour ${file}..."
+        sh "sudo /home/ndama/jmeter/apache-jmeter-5.6.3/bin/jmeter -n -t ${file} -l results.jtl"
+        
+        // Générer un rapport de performance
+        perfReport "results.jtl"
     }
 }
