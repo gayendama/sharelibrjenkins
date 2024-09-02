5ccc24786b9a8de4cabc203defbcc78a59312c80
gayendama <117781785+gayendama@users.noreply.github.com>
2024-09-02 10:22:22 +0000

    Update runSoapUITests.groovy

--- vars/runSoapUITests.groovy
+++ vars/runSoapUITests.groovy
@@ -9,14 +9,14 @@ def call() {
     // Vérifier si le répertoire de tests existe
     if (!fileExists(soapuiDir)) {
     echo "Le répertoire des tests SoapUI ${soapuiDir} n'existe pas, aucun test ne sera effectué."
-    return
+    return false
 } else {
     // Trouver tous les fichiers .xml dans le répertoire de tests et les exécuter
     def soapuiFiles = sh(script: "find ${soapuiDir} -name '*.xml'", returnStdout: true).trim().split('\n')
 
     if (soapuiFiles.size() == 0 || soapuiFiles[0] == "") {
         echo "Aucun fichier SoapUI trouvé dans ${soapuiDir}."
-        return
+        return false
     }
 
     echo "Exécution des fichiers SoapUI trouvés : ${soapuiFiles.join(', ')}"
@@ -26,6 +26,7 @@ def call() {
         echo "Exécution des tests SoapUI pour ${file}..."
         sh "/opt/SmartBear/SoapUI-5.7.2/bin/testrunner.sh -r -j -f ${resultsDir} ${file}"
     }
+        return true
     }    
 }
 
