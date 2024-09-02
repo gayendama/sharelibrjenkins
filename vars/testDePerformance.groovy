9ba9c4fb6c762f3b97c728111643c72f051ed616
gayendama <117781785+gayendama@users.noreply.github.com>
2024-09-02 12:22:59 +0000

    Update testDePerformance.groovy

--- vars/testDePerformance.groovy
+++ vars/testDePerformance.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
 def call() {
     def appDir = "${env.WORKSPACE}/" 
-    def appDir = "${env.WORKSPACE}/" 
+   
    
     def jmxFile = sh(script: "ls ${appDir}*.jmx | head -n 1", returnStdout: true).trim()
 
      if (jmxFile) {
         echo "Fichier .jmx trouvé : ${jmxFile}"
-        sh "sudo /opt/apache-jmeter-5.6.3/bin/jmeter  -n -t ${jmxFile} -l results.jtl"
+        sh "sudo /home/ndama/jmeter/apache-jmeter-5.6.3/bin/jmeter  -n -t ${jmxFile} -l results.jtl"
         perfReport 'results.jtl'
     } else {
         echo "Aucun fichier .jmx trouvé dans le répertoire ${appDir}, aucun test ne sera exécuté."
