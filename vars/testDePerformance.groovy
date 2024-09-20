9364a68b6fa533e7ca546fa006e0fba780412040
gayendama <117781785+gayendama@users.noreply.github.com>
2024-09-20 09:17:31 +0000

    Update testDePerformance.groovy

--- vars/testDePerformance.groovy
+++ vars/testDePerformance.groovy
@@ -3,7 +3,7 @@ def call() {
     def appDir = "${env.WORKSPACE}/" 
     //def resultsDir = "/home/ndama/jmeter-reports"
     // Chemin vers le répertoire contenant les fichiers jmeter
-    def jmeteriDir = "/home/ndama/testjmeter/"
+    def jmeteriDir = "/home/ndama/testjmeter"
     // Créer le répertoire pour les rapports s'il n'existe pas
     //sh "mkdir -p ${resultsDir}"
     // Vérifier si le répertoire de tests existe
@@ -11,8 +11,8 @@ def call() {
     echo "Le répertoire des tests jmeter ${jmeteriDir} n'existe pas, aucun test ne sera effectué."
     return false
 } else {
-    // Trouver tous les fichiers .xml dans le répertoire de tests et les exécuter
-    def jmeterFiles = sh(script: "find ${jmeteriDir} -name '*.xml'", returnStdout: true).trim().split('\n')
+    // Trouver tous les fichiers .jmx dans le répertoire de tests et les exécuter
+    def jmeterFiles = sh(script: "find ${jmeteriDir} -name '*.jmx'", returnStdout: true).trim().split('\n')
 
     if (jmeterFiles.size() == 0 || jmeterFiles[0] == "") {
         echo "Aucun fichier jmeter trouvé dans ${jmeteriDir}."
